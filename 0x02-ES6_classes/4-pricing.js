eb2d51a1befa2bcb6dd833ff1d6723320041518e
Irungu Macharia <machariajosphat001@gmail.com>
2023-06-29 18:05:17 +0000

    task 4

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,36 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = new Currency(currency.code, currency.name);
+  }
+
+  // Get and set 'amount'
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(newAmount) {
+    this._amount = newAmount;
+  }
+
+  // Get and set 'Currency'
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(newCurrency) {
+    this._currency = new Currency(newCurrency.code, newCurrency.name);
+  }
+
+  // display  in the following format amount currency_name (currency_code).
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  // convert price
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}

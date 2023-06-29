a6e845d2de0ec3315924f4245a158a3fa6c351de
Irungu Macharia <machariajosphat001@gmail.com>
2023-06-29 17:01:04 +0000

    class init and 01

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,13 @@
+import ClassRoom from './0-classroom';
+
+export default function initializeRooms() {
+  const classrooms = [];
+
+  const c1 = new ClassRoom(19);
+  const c2 = new ClassRoom(20);
+  const c3 = new ClassRoom(34);
+
+  classrooms.push(c1, c2, c3);
+
+  return (classrooms);
+}

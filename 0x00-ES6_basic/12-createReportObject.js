8cd2ccc96ea0ec2f2905a675def933088388ad73
Irungu Macharia <machariajosphat001@gmail.com>
2023-07-02 13:41:07 +0000

    11 empl2 report object

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,3 +1,12 @@
 export default function createReportObject(employeesList) {
+  const reportObject = {
+    allEmployees: {
+      ...employeesList,
+    },
+    getNumberOfDepartments() {
+      return Object.keys(this.allEmployees).length;
+    },
+  };
 
+  return reportObject;
 }

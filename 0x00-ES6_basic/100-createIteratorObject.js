8630325b4a91e9d54bbede5cf03bc2e504c07a9e
Irungu Macharia <machariajosphat001@gmail.com>
2023-07-02 13:58:32 +0000

    iterator

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,3 +1,30 @@
 export default function createIteratorObject(report) {
+  let departmentIndex = 0;
+  let employeeIndex = 0;
+  const departments = Object.keys(report.allEmployees);
 
+  const iterator = {
+    next() {
+      const department = departments[departmentIndex];
+      const employees = report.allEmployees[department];
+      const employee = employees[employeeIndex];
+
+      if (employeeIndex < employees.length - 1) {
+        employeeIndex += 1;
+      } else {
+        employeeIndex = 0;
+        departmentIndex += 1;
+      }
+
+      if (departmentIndex >= departments.length) {
+        return { done: true };
+      }
+
+      return { value: employee, done: false };
+    },
+
+    [Symbol.iterator]: () => iterator,
+  };
+
+  return iterator;
 }

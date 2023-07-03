ed8087c4633f43707f66130a632741c5bfd194b3
Irungu Macharia <machariajosphat001@gmail.com>
2023-07-03 18:25:45 +0000

    es6 data manipulation task 4 done

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,3 +1,20 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
-  
+  const cityStudents = students.filter((student) => student.location === city);
+
+  return cityStudents.map((student) => {
+    const updatedStudent = { ...student };
+    let foundGrade = false;
+    newGrades.forEach((gradeObj) => {
+      if (gradeObj.studentId === student.id) {
+        updatedStudent.grade = gradeObj.grade;
+        foundGrade = true;
+      }
+    });
+
+    if (!foundGrade) {
+      updatedStudent.grade = 'N/A';
+    }
+
+    return updatedStudent;
+  });
 }

2e50a270444f756f6954fb96e9b28ec5552b10ea
MrMvshv <machariajosphat001@gmail.com>
2023-08-21 18:06:44 +0000

    promise

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,16 @@
+import uploadPhoto from './5-photo-reject';
+import signUpUser from './4-user-promise';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const signUpUserPromise = signUpUser(firstName, lastName);
+  const uploadPhotoPromise = uploadPhoto(fileName);
+
+  return Promise.allSettled([signUpUserPromise, uploadPhotoPromise])
+    .then((results) => results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason,
+    })))
+    .catch((error) => {
+      throw error;
+    });
+}

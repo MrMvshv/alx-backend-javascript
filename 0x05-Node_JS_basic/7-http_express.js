428e99a08d1787111dbf564e45d969c1f65d1055
Irungu Macharia <machariajosphat001@gmail.com>
2023-08-29 10:35:07 +0000

    manual lint

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -2,18 +2,33 @@ const express = require('express');
 const countStudents = require('./3-read_file_async');
 
 const app = express();
+const PORT = 1245;
 
 app.get('/', (req, res) => res.send('Hello Holberton School!'));
 app.get('/students', async (req, res) => {
   const initialHeaderResponse = 'This is the list of our students\n';
-  countStudents(process.argv[2])
-    .then((response) => {
-      const message = `${initialHeaderResponse}${response}`;
-      res.send(message);
-    })
-    .catch((err) => res.send(`${initialHeaderResponse}${err.message}`));
+
+  try {
+    const fields = await countStudents(process.argv[2]);
+
+    // Calculate the total number of students
+    const totalStudents = Object.values(fields).reduce((total, field) => total + field.length, 0);
+
+    let response = `${initialHeaderResponse}Number of students: ${totalStudents}\n`;
+
+    Object.keys(fields).forEach((field) => {
+      response += `Number of students in ${field}: ${fields[field].length}.`;
+      response += ` List: ${fields[field].join(', ')}\n`;
+    });
+
+    res.send(response);
+  } catch (err) {
+    res.send(`${initialHeaderResponse}${err.message}`);
+  }
 });
 
-app.listen(1245);
+app.listen(PORT, () => {
+  console.log('...');
+});
 
 module.exports = app;

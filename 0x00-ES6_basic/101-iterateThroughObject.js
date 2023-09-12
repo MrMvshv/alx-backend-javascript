c0f05162b4f81f29c42dc67ca590bc664162d551
MrMvshv <machariajosphat001@gmail.com>
2023-09-12 16:49:55 +0000

    user auth

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -2,8 +2,8 @@ export default function iterateThroughObject(reportWithIterator) {
   const employeeNames = [];
 
   for (const item of reportWithIterator) {
-      employeeNames.push(item);
-    }
+    employeeNames.push(item);
+  }
 
   return employeeNames.join(' | ');
 }
